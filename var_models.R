bd45c30ef052657347b15006c24c5f9c4c2806f2
Wilbert <wilbertosmond99@gmail.com>
2021-03-21 23:53:38 +0100

    fixed bugs

--- var_models.R
+++ var_models.R
@@ -5,14 +5,11 @@ library("mlVAR")
 
 setwd("C:/Users/wilbert osmond/Documents/Internship - Thesis 2.0/VAR Models (R)")
 
-#### Data preparation ----
-# Read the file into R:
-Data1 <- read_excel("Patient20.xlsx")
 
-#### Select EMA items as symptoms. Variables to investigate:
+#### Task 1: graphicalVAR model ===============
+Data1 <- read_excel("Patient20.xlsx")
 Vars <- c("dh51", "dh52", "dh53", "dh54", "dh58")
 
-#### Task 2: graphicalVAR model ===============
 Results <- graphicalVAR(
   Data1,
   idvar = "unique_id",
@@ -28,10 +25,11 @@ Results$PDC
 
 #### Task 2: mlVAR model ===============
 Data2<-read_excel("df_EMA_cleaned.xlsx")
-head(Data2)
+vars <- c("dh51", "dh52", "dh53", "dh54", "dh58")
 length(unique(Data2$ID)) # number of participants
 
-res <- mlVAR(Data4, Vars, idvar="ID", dayvar="day", beepvar="beep", 
+res <- mlVAR(Data2, vars, idvar="ID", dayvar="day", beepvar="beep", 
              lags = 1, temporal = "correlated", contemporaneous = "correlated")
 
 plot(res, "temporal", layout = "circle", nonsig = "hide")
+summary(res)
